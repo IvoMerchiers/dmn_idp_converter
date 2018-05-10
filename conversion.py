f5b572f7224440a33922ac63620fa27915472908
IvoMerchiers <ivomerchiers@users.noreply.github.com>
2018-05-10 12:57:17 +0200

    i

--- conversion.py
+++ conversion.py
@@ -2,15 +2,15 @@ import xml.etree.ElementTree as ET
 from dmnconverter.converter import DMNConverter as Dmn
 
 # --- values---
-directory='..\DMNTables\\'
-file_name = "RunningExample"
+directory = '..\DMNTables\\'
+file_name = "ChainBBQ"
 
 # fileName = "..\DMNTables\BepaalTypeWoningBoolean.dmn"
-#outputName = 'RunningExample.idp'
+# outputName = 'RunningExample.idp'
 
 # ---- Script----
-file_location=directory+file_name+'.dmn'
-output_name=file_name+'.idp'
+file_location = directory + file_name + '.dmn'
+output_name = file_name + '.idp'
 conv = Dmn()
 conv.read(file_location)
 conv.print_meta(output_name)
